53509bdc90bf07331f911a97da841076a4351444
zengqi <zengqi@realibox.com>
2020-07-23 13:48:24 +0800

    msg

--- app/static/web_html.py
+++ app/static/web_html.py
@@ -17,7 +17,7 @@ html = """
         <script>
             document.getElementById("messageText").placeholder="第一次输入内容为昵称";
         
-            var ws = new WebSocket("ws://39.107.77.70:8888/ws");
+            var ws = new WebSocket("ws://127.0.0.1:8021/ws");
             
             // 接收
             ws.onmessage = function(event) {
